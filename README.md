fca572a8ffab6450ba1a9b5b327b66fdc43c83c4
CyberNika <cybernika.net@gmail.com>
2017-11-07 22:55:37 +0800

    first blood

---
+++ README.md
@@ -0,0 +1,3 @@
+# v-resize
+
+适用于 Vue 2.0 的 Resize 指令。
---
+++ examples/App.vue
@@ -0,0 +1,31 @@
+<template>
+  <div id="app">
+    <div class="box" v-resize>
+
+    </div>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'App',
+    data () {
+      return {
+      }
+    }
+  }
+</script>
+
+<style scoped>
+  .box {
+    width: 300px;
+    height: 200px;
+    border: 1px solid orange;
+  }
+</style>
+
+<style>
+  html, body {
+    height: 100%;
+  }
+</style>
